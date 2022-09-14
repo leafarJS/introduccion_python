b091b0f6ba38378a4288d9d538cdcaee39cc59d4
unknown <riskeconomics@gmail.com>
2022-09-14 16:54:31 -0400

    quinto commit bucle while

---
+++ 09_bucle_while.py
@@ -0,0 +1,16 @@
+#bucle while
+# ? ciclo que continua mientras una condición es
+# ? verdadera y se detiene cuando es falsa 
+
+x = 20
+while x < 35:
+  print(x)
+  x+=3
+
+print("_____________")
+tupla = (1, 2, 3, 4, 5, 6, 7, 8)
+
+y = 20
+while y > len(tupla):
+  print(y)
+  y-=2
